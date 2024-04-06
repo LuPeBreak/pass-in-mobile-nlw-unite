d794263fe325ed90e03c2915476065cd13adde28
LuPeBreak <lfbmrj15@gmail.com>
2024-04-06 11:22:36 -0300

    Project Start

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        backgroundColor: "red",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Hello React Native</Text>
+    </View>
+  );
+}
